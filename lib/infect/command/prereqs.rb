4a1269fc21953846f6b0ad3f7d6cff11881749c7
Christopher Sexton <csexton@gmail.com>
2013-03-27 11:57:06 -0400

    Update directory creation for sensible.vim

--- lib/infect/command/prereqs.rb
+++ lib/infect/command/prereqs.rb
@@ -2,11 +2,18 @@ module Infect
   class Command
     class Prereqs < Command
       def call
-        mkdir "~/.vim/bundle"
-        # sensible.vim keeps these under ~/.cache
-        mkdir "~/.cache/swap"
-        mkdir "~/.cache/backup"
-        mkdir "~/.cache/undo"
+        mkdir_p "~/.vim/bundle"
+        # create the cache directories for sensible.vim:
+        # TODO: Support Windows
+        if RUBY_PLATFORM =~ /darwin/
+          mkdir_p "~/Library/Vim/swap"
+          mkdir_p "~/Library/Vim/backup"
+          mkdir_p "~/Library/Vim/undo"
+        else
+          mkdir_p "~/.local/share/vim/swap"
+          mkdir_p "~/.local/share/vim/backup"
+          mkdir_p "~/.local/share/vim/undo"
+        end
       end
     end
   end
